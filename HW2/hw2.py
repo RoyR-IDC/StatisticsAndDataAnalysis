3f8030ca78c25771cf4e2662783f1bd9f0c72693
livneor207 <livneor207@gmail.com>
2021-11-25 17:23:28 +0200

    .....

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -54,11 +54,152 @@ add two horizontal lines on the first and third quartile (on the cumulative coun
 6. Identify and report 2 “interesting” trends in the data. 
 No need to provide statistical confidence at this point. 
 """
+#SETUP
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.gridspec import GridSpec
+from scipy import stats
+from sklearn import mixture
+plt.style.use('default') # You need to "reset" befoe you switch styles.
+plt.style.use('seaborn-pastel')
+mpl.rcParams['figure.figsize'] = [6, 4]
+from sklearn.datasets import load_boston
+boston = load_boston()
+
+X = boston.data
+columns = list(boston.feature_names)
+y = boston.target
+boston_df = pd.DataFrame(np.concatenate((X, y[:, np.newaxis]), axis=1), \
+                         columns= columns + ['PRICE'])
+#boston_df.head()
+print(boston_df.to_markdown())
+
+#1. describe the dataset. How many samples does it contain? 
+#   How many features? What isis the data type for each variable?
+
+#
+amount_of_feature = boston_df.shape[1]-1
+amount_of_sampels = boston_df.shape[0]
+print('The amount of sampels is: ' + str(amount_of_sampels))
+print('The amount of feature is: ' + str(amount_of_feature))
+print('data type for each feature:')
+boston_df.info(verbose=True)
+
+#2. Produce a histogram and a boxplot of the nitric oxides concentration. 
+#   describe the distribution.
+boston_df_col = boston_df.columns.to_numpy()
+
+plt.figure()
+ax = sns.histplot(x="NOX", data=boston_df)
+plt.title('histogram plot of NOX feature')
+plt.grid()
+NOX_feature = boston_df['NOX'].to_list()
+plt.axvline(x = 0.44, color= 'r')
+plt.axvline(x = 0.66, color= 'r')
+
+np.mean(NOX_feature)
+np.std(NOX_feature)
+np.median(NOX_feature)
+
+"""
+we can see from the histogram plot that most of the data in the range 
+of [mean-std,mean+std]
+in our case the mean is 0.55 and the std is 0.11 -> [0.44,0.66]
+"""
+
+plt.figure()
+ax = sns.boxplot(x="NOX", data=boston_df)
+plt.title('box plot of NOX feature')
+#plt.grid()
+"""
+the box plot show the precentiles:
+    1. the first line describe the 0% percentile (the min value)
+    2. the secound line describe the 25% percentile (Q1)
+    3. the third line describe the 50% percentile (median) 
+    4. the forth line describe the 75% percentile (Q3) 
+    5. the fivth line describe the 100% percentile (the max value) 
+from our box plot we can infer that the IQR(inter quartile range) is
+between ~0.44 to ~0.66 which correlate with the histogram conclution 
+"""
+
+#3. Produce a correlation matrix of all the features. 
+#   [Are there any correlated features?
+
+corr_array = boston_df.corr().to_numpy()
+np.fill_diagonal(corr_array, 0)
+max_corr_row_col = np.where(corr_array == np.max(corr_array))[0]
+min_corr_row_col = np.where(corr_array == np.min(corr_array))[0]
+min_cor_features = boston_df_col[min_corr_row_col].tolist()
+max_cor_features = boston_df_col[max_corr_row_col].tolist()
 
 
 
-#sss
 
+
+plt.figure()
+sns.heatmap(boston_df.corr(),cmap='coolwarm', vmin=-1, vmax=1)
+plt.title('correlation matrix of all the features.')
+
+string = 'the maximum correlation is between each feature with himself in the digonal'
+print(string)
+string = 'The min corroletion found to be betwenn feature ' + min_cor_features[0] + ' to feature ' \
++ min_cor_features[1]
+print(string)
+
+string = 'The max corroletion found to be betwenn feature ' + max_cor_features[0] + ' to feature ' \
++ max_cor_features[1]
+print(string)
+
+string = 'The unusual feature is the CHAS, it can be seen from the heat map,' + \
+        'that he has no correlation with other features'
+print(string)
+#4. Select the 2 pairs of features with the highest correlation
+# (positive or negative) and plot 2 scatter plots with marginal histograms (JointPlot). 
+
+
+
+ax= sns.jointplot(x=boston_df[max_cor_features[0]], y=boston_df[max_cor_features[1]],\
+              marginal_kws=dict(bins=30))
+ax.fig.suptitle('Joint plot between ' + max_cor_features[0]+ ', '+ max_cor_features[1])
+
+ax= sns.jointplot(x=boston_df[min_cor_features[0]], y=boston_df[min_cor_features[1]],\
+              marginal_kws=dict(bins=30))
+ax.fig.suptitle('Joint plot between ' + min_cor_features[0]+ ', '+ min_cor_features[1])
+
+
+#5. Produce a cumulative histogram of the age variable and 
+#   add two horizontal lines on the first and third quartile (on the cumulative count)
+
+plt.figure()
+ax = sns.histplot(x="AGE", data=boston_df, cumulative=True, stat='density')
+plt.title('cumulative histogram plot of AGE feature')
+cumsum = boston_df['AGE'].to_numpy().cumsum()
+plt.axhline(y= 0.25, color = 'r')
+plt.axhline(y= 0.75, color = 'r')
+plt.grid()
+
+#6. Identify and report 2 “interesting” trends in the data. 
+#   No need to provide statistical confidence at this point. 
+
+sns.scatter(x='index', y= 'RM', hue ='PRICE', data= boston_df.reset_index(0))
+boston_df.plot.scatter(x='RM', y='PRICE')
+"""
+
+
+"""
+"""
+the box plot show the precentiles:
+    1. the first line describe the 0% percentile (the min value)
+    2. the secound line describe the 25% percentile (Q1)
+    3. the third line describe the 50% percentile (median) 
+    4. the forth line describe the 75% percentile (Q3) 
+    5. the fivth line describe the 100% percentile (the max value) 
+from our box plot we can infer that the IQR(inter quartile range) is
+between ~0.44 to ~0.66 which correlate with the histogram conclution 
+"""
 """
 # ------------------------------------------------------------------------------------------------------------
 
